d1f10b97720d3dcfbf59a8281fbd7353a10ced28
simonBtssio <simon.farnault.sio@gmail.com>
2022-01-31 15:30:01 +0100

    connexion db, fonction prepare
    Connexion avec la base de donnée établie et création de la fontion prepare qui permettra de faire les différentes requétes necessaire

--- www/singleton/DB.class.php
+++ www/singleton/DB.class.php
@@ -1,4 +1,26 @@
 <?php
     class DB {
+        private static $instance = null;
+        private $conn;
+         
+        // La connexion au bd est établie dans le constructeur privé.
+        private function __construct()
+        {
+          $this->conn = new PDO("mysql:host=localhost; dbname=test", "myuser", "mypassword");
+        }
         
+        public static function getInstance()
+        {
+          if(!self::$instance)
+          {
+            self::$instance = new ConnexionBD();
+          }
+         
+          return self::$instance;
+        }
+        
+        public function getConnection()
+        {
+          return $this->conn;
+        }
     }
